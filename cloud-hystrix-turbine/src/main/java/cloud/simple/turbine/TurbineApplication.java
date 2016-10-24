6e955199988cb28c4ffc8efbfe9bf41d57ad187a
xuhj <359760683@qq.com>
2016-10-24 09:32:35 +0800

    code format

--- cloud-hystrix-turbine/src/main/java/cloud/simple/turbine/TurbineApplication.java
+++ cloud-hystrix-turbine/src/main/java/cloud/simple/turbine/TurbineApplication.java
@@ -8,17 +8,15 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 
-
-
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
 @EnableTurbine
 public class TurbineApplication {
 
-	public static void main(String[] args) {
-		 SpringApplication.run(TurbineApplication.class, args);
-		//boolean cloudEnvironment = new StandardEnvironment().acceptsProfiles("cloud");
-		//new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+        //boolean cloudEnvironment = new StandardEnvironment().acceptsProfiles("cloud");
+        //new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
+    }
 }
